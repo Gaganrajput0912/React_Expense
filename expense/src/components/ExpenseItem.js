14e258f21b9553713363c9aeb8db260fb37986f6
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-21 17:24:40 +0530

    first commit

--- expense/src/components/ExpenseItem.js
+++ expense/src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
-function ExpenseItem(){
-    return <div>
-        <ul>
-            <li>Food Rs 10</li>
-            <li>Petrol Rs 100</li>
-            <li>Movies Rs 200</li>
-        </ul>
+import './ExpenseItem.css'
+function ExpenseItem(props){
+    // const expenseDate  = new Date(2021,2,28)
+    // const expenseTitle = 'Car Insurance'
+    // const expenseAmount = 296
+    // const LocationOfExpenditure="New-Delhi"
+    return (<div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description"><h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <div>{props.LocationOfExpenditure}</div></div>
     </div>
+    );
 }
 
 export default ExpenseItem;
