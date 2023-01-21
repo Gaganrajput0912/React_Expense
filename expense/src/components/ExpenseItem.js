0b2494d23b3be97d3da213e248bed9cbdca864ba
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-21 11:18:50 +0530

    first commit

--- expense/src/components/ExpenseItem.js
+++ expense/src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem(){
-    return <h2>Expense item!</h2>
+    return <div>
+        <ul>
+            <li>Food Rs 10</li>
+            <li>Petrol Rs 100</li>
+            <li>Movies Rs 200</li>
+        </ul>
+    </div>
 }
 
 export default ExpenseItem;
