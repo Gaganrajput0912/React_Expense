772fa7cbb4f9f594733b39cd76cac399709b7180
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-22 12:18:54 +0530

    first commit

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,6 +1,7 @@
 
-import ExpenseItem  from "./components/ExpenseItem";
-function App() {
+
+import Expenses from "./components/Expenses/Expenses";
+const App =() => {
   const expenses = [
     {
       id: 'e1',
@@ -26,23 +27,13 @@ function App() {
       LocationOfExpenditure:"Agra",
     },
   ];
+ 
   return (
-    <div className="App">
-     <h2>REACT</h2>
-     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount}
-     date={expenses[0].date}
-     LocationOfExpenditure={expenses[0].LocationOfExpenditure}></ExpenseItem>
-     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount}
-     date={expenses[1].date}
-     LocationOfExpenditure={expenses[1].LocationOfExpenditure}></ExpenseItem>
-     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount}
-     date={expenses[2].date}
-     LocationOfExpenditure={expenses[2].LocationOfExpenditure}></ExpenseItem>
-     <ExpenseItem title={expenses[3].title} amount={expenses[3].amount}
-     date={expenses[3].date}
-     LocationOfExpenditure={expenses[3].LocationOfExpenditure}></ExpenseItem>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
 export default App; 
--- expense/src/components/Expenses/ExpenseItem.js
+++ expense/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
 import './ExpenseItem.css'
+import Card from '../UI/Card';
+
+
 import ExpenseDate from './ExpenseDate';
-function ExpenseItem(props){
+const ExpenseItem = (props) => {
     // const expenseDate  = new Date(2021,2,28)
     // const expenseTitle = 'Car Insurance'
     // const expenseAmount = 296
     // const LocationOfExpenditure="New-Delhi"
     
-    return (<div className="expense-item">
+    return (<Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description"><h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <div>{props.LocationOfExpenditure}</div></div>
-    </div>
+        <div className="expense-item__location">{props.LocationOfExpenditure}</div></div>
+    </Card>
     );
 }
 
