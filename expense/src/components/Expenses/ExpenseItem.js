a8be2dca451ef86288e60680bdf37108e64051a8
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-24 18:20:55 +0530

    first commit

--- expense/src/components/Expenses/ExpenseItem.js
+++ expense/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,16 @@ import Card from '../UI/Card';
 
 import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
+    const clickHandler = () =>{
+
+
+
+    }
+    const deleteE = (e)=>{
+
+
+    }
+// document.getElementById('root').addEventListener()
     // const expenseDate  = new Date(2021,2,28)
     // const expenseTitle = 'Car Insurance'
     // const expenseAmount = 296
@@ -14,6 +24,8 @@ const ExpenseItem = (props) => {
         <div className="expense-item__description"><h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <div className="expense-item__location">{props.LocationOfExpenditure}</div></div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteE}>Delete</button>
     </Card>
     );
 }
