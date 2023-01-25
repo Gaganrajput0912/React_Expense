32b35999c03200eab7b9be8c4f2efe157858b53f
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-25 17:39:13 +0530

    first commit

--- expense/src/components/Expenses/ExpenseItem.js
+++ expense/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card';
-
+import React,{useState} from 'react'
 
 import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
+    const [title,setTitle] = useState(props.title)
+    const [amount,ChangeAmount] = useState(props.amount)
+    // let title  = props.title;
     const clickHandler = () =>{
-
-
+    // title='Updated'
+    setTitle('Updated')
 
     }
     const deleteE = (e)=>{
 
 
+    }
+
+    const Change = () =>{
+        ChangeAmount('100')
     }
 // document.getElementById('root').addEventListener()
     // const expenseDate  = new Date(2021,2,28)
@@ -21,8 +28,9 @@ const ExpenseItem = (props) => {
     
     return (<Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
-        <div className="expense-item__description"><h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__description"><h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={Change}>Change</button>
         <div className="expense-item__location">{props.LocationOfExpenditure}</div></div>
         <button onClick={clickHandler}>Change Title</button>
         <button onClick={deleteE}>Delete</button>
