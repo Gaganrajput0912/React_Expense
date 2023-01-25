fc649ef19eee89652cc9b2b6a39d70db9517b8fd
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-01-25 23:13:48 +0530

    first commit

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,5 +1,5 @@
 
-
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 const App =() => {
   const expenses = [
@@ -30,7 +30,8 @@ const App =() => {
  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
+      
       <Expenses items={expenses} />
     </div>
   )
